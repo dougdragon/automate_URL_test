4f067e41d007b3873f9a9d072210a79aa6dab6a5
Doug Dragon <thatdougore@gmail.com>
2013-08-07 09:52:58 -0400

    Removed text file.

--- test_wsdls.py
+++ test_wsdls.py
@@ -13,52 +13,58 @@ def main():
         log = open('logging.txt', 'a')
     except IOError:
         log = open('logging.txt', 'w')
-    print "Reading the URLS from the wsdls.txt file..."
-    for line in open('wsdls.txt', 'r').readlines():
-        urls.append(line.strip())
-    print "Done."
-    print "Hitting the WSDL URLs now:"
-    log.writelines('---------------------------------------------------------------------------------------------------------\r\n')
+    print "Reading the URLs from the wsdls.txt file..."
+    try:
+        for line in open('urls.txt', 'r').readlines():
+            urls.append(line.strip())
 
-    for url in urls:
-        try:
-            r = urllib2.urlopen(url) # urllib
-            passed_count += 1
-        except URLError, e:
-            failed_count += 1
-            failed_urls.append(url)
-            log.writelines("%s - FAILED: %s.\r\n" % (str(time.now()), url))
-            log.writelines("Error code: %s \r\n" % e.code)
-            print ""
-            print "Failed:"
-            print "-------"
-            print ""
-            print "URL: %s" % url
-            print "URL returned %s." % e.code # urllib
-            print ""
+        print "Done."
+        print "Hitting the URLs now:"
+        log.writelines('---------------------------------------------------------------------------------------------------------\r\n')
 
-    end = time.now()
-    total_count = failed_count + passed_count
+        for url in urls:
+            try:
+                r = urllib2.urlopen(url)
+                passed_count += 1
+            except URLError, e:
+                failed_count += 1
+                failed_urls.append(url)
+                log.writelines("%s - FAILED: %s.\r\n" % (str(time.now()), url))
+                log.writelines("Error code: %s \r\n" % e.code)
+                print ""
+                print "Failed:"
+                print "-------"
+                print ""
+                print "URL: %s" % url
+                print "URL returned %s." % e.code # urllib
+                print ""
 
-    log.writelines('                  ------------- Summary -----------\r\n')
-    log.writelines('Total URLs tested: %s -- ' % total_count)
-    log.writelines('Passed URLs: %s -- ' % passed_count)
-    log.writelines('Failed URLs: %s -- ' % failed_count)
-    log.writelines('---------------------------------------------------------------------------------------------------------\r\n')
+        end = time.now()
+        total_count = failed_count + passed_count
 
-    print ""
-    print "---------------------------------------"
-    print "Total URLs tested: %s." % total_count
-    print "Passed URLs: %s." % passed_count
-    print "Failed URLs: %s." % failed_count
-    print ""
-    print "Failed URLs:"
-    print "============="
-    for url in failed_urls:
-        print url
-    print "---------------------------------------"
-    print ""
-    print "Test time: %s." % (end - begin)
+        log.writelines('                  ------------- Summary -----------\r\n')
+        log.writelines('Total URLs tested: %s -- ' % total_count)
+        log.writelines('Passed URLs: %s -- ' % passed_count)
+        log.writelines('Failed URLs: %s -- ' % failed_count)
+        log.writelines('---------------------------------------------------------------------------------------------------------\r\n')
+
+        print ""
+        print "---------------------------------------"
+        print "Total URLs tested: %s." % total_count
+        print "Passed URLs: %s." % passed_count
+        print "Failed URLs: %s." % failed_count
+        print ""
+        print "Failed URLs:"
+        print "============="
+        for url in failed_urls:
+            print url
+        print "---------------------------------------"
+        print ""
+        print "Test time: %s." % (end - begin)
+
+    except IOError:
+        print "IOError: Could not locate 'urls.txt' file."
+        log.writelines("\r\n%s - IOError: Could not locate 'urls.txt' file." % (str(time.now())))
 
 if __name__ == "__main__":
     main()
